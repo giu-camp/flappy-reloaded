075ea008d56f22acf6b75b5d5edc4ccec35d24b1
Antonio Alexandru Ganea <antonio.ganea@yahoo.com>
2020-04-11 20:06:03 +0300

    Movingtubes (#9)
    * Moving a tube to the left
    * Added moving tubes array
    * Randomized tubes positions

--- src/states/game.ts
+++ src/states/game.ts
@@ -7,11 +7,22 @@ export class Game extends Phaser.State {
     private speedY = 0;
     private gravity = 0.1;
 
+    private tubesArray = [];
+
     public create(): void {
         this.bird = this.game.add.sprite(75, 100, "bird");
         this.bird.anchor.setTo(0.5);
 
-        const tube = new Tube(this.game, 300, 600);
+        for ( let i = 0; i < 10; i++ ) {
+            const currentTube = new Tube(this.game, 600 + i * 300, 600);
+
+            if ( Math.random() < 0.5 ) {
+                currentTube.angle = 180;
+                currentTube.y -= 750;
+            }
+
+            this.tubesArray.push ( currentTube );
+        }
 
         this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         this.spaceKey.onDown.add(() => {
@@ -24,6 +35,10 @@ export class Game extends Phaser.State {
         this.bird.y += this.speedY;
 
         this.bird.angle = this.speedY * 5;
+
+        for ( let i = 0; i < this.tubesArray.length; i++ ) {
+            this.tubesArray[i].x -= 5;
+        }
     }
 
 
