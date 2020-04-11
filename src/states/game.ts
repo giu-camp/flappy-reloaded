77ba91c1d3ecabaec688b234871cbcaa0a9b7c8b
Antonio Ganea <antonio.ganea@yahoo.com>
2020-04-11 19:28:19 +0300

    Added functionality to the flappy game

--- src/states/game.ts
+++ src/states/game.ts
@@ -1,8 +1,27 @@
 import { Tube } from "../prefabs/tube";
 
 export class Game extends Phaser.State {
+
+    private bird;
+    private spaceKey: Phaser.Key;
+    private speedY = 0;
+    private gravity = 0.1;
+
     public create(): void {
-        const bird = this.game.add.sprite(50, 150, "bird");
+        this.bird = this.game.add.sprite(50, 100, "bird");
+
         const tube = new Tube(this.game, 300, 600);
+
+        this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        this.spaceKey.onDown.add(() => {
+            this.speedY = -5;
+        }, this);
     }
+
+    public update() {
+        this.speedY += this.gravity;
+        this.bird.y += this.speedY;
+    }
+
+
 }
