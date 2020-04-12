f6da0fbda7cfd52970c66131b1b692f698c6cc3e
TeoThatsMe <45195314+TeoThatsMe@users.noreply.github.com>
2020-04-12 10:22:30 +0300

    Tubes (#13)
    * Added .vs/ to .gitignore
    
    * Tubes are now on the ceiling and on the floor

--- src/states/game.ts
+++ src/states/game.ts
@@ -7,7 +7,8 @@ export class Game extends Phaser.State {
     private speedY = 0;
     private gravity = 0.1;
 
-    private tubesArray = [];
+    private floorTubesArray = [];
+    private ceilingTubesArray = [];
 
     private background;
 
@@ -19,14 +20,16 @@ export class Game extends Phaser.State {
         this.bird.anchor.setTo(0.5);
 
         for ( let i = 0; i < 10; i++ ) {
+
             const currentTube = new Tube(this.game, 600 + i * 300, 600);
+            this.floorTubesArray.push ( currentTube );
+        }
+        for ( let i = 0; i < 10; i++ ) {
 
-            if ( Math.random() < 0.5 ) {
-                currentTube.angle = 180;
-                currentTube.y -= 750;
-            }
-
-            this.tubesArray.push ( currentTube );
+            const currentTube = new Tube(this.game, 600 + i * 300, 600);
+            currentTube.angle = 180;
+            currentTube.y -= 750;
+            this.ceilingTubesArray.push ( currentTube );
         }
 
         this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -41,8 +44,9 @@ export class Game extends Phaser.State {
 
         this.bird.angle = this.speedY * 5;
 
-        for ( let i = 0; i < this.tubesArray.length; i++ ) {
-            this.tubesArray[i].x -= 5;
+        for ( let i = 0; i < this.floorTubesArray.length; i++ ) {
+            this.floorTubesArray[i].x -= 5;
+            this.ceilingTubesArray[i].x -= 5;
         }
 
         this.background.x -= 0.5;
